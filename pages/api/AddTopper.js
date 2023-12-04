0a23304b5c7fb75e9626aac6f4e92140a9936c43
prathamupadhyay-tech <pratham27900@gmail.com>
2023-12-04 11:48:44 +0530

    added error checks

--- pages/api/AddTopper.js
+++ pages/api/AddTopper.js
@@ -35,7 +35,8 @@ router
     let files;
     try {
       [fields, files] = await form.parse(req);
-
+      console.log(fields);
+      console.log(files);
       await saveTopper(fields, files);
       res.status(200).json({ message: "saved" });
     } catch (err) {
@@ -61,46 +62,49 @@ router
   });
 
 const saveTopper = async (fields, files) => {
-  if (!files) {
-    return res.status(400).json({ message: "Image file is required" });
+  try {
+    if (!files) {
+      return res.status(400).json({ message: "Image file is required" });
+    }
+
+    const compressedBuffer = await sharp(files.profileImage[0].filepath)
+      .resize({ width: 250, height: 250 })
+      .withMetadata()
+      .jpeg({ quality: 80 }) // Adjust quality as needed
+      .toBuffer();
+
+    // const filename = new Date().getTime() + "-" + req.file.originalname;
+    // const newFilePath = path.join(UPLOAD_PATH, filename);
+
+    // const buffer = await sharp(req.file.buffer)
+    //   .resize()
+    //   .jpeg({ quality: 50 })
+    //   .toBuffer();
+
+    const image64 = compressedBuffer.toString("base64");
+
+    let newTopper = new topper({
+      name: fields.name[0],
+      rank: parseInt(fields.rank[0]),
+      year: parseInt(fields.year[0]),
+      gs1marks: parseInt(fields.gs1marks[0]),
+      gs2marks: parseInt(fields.gs2marks[0]),
+      gs3marks: parseInt(fields.gs3marks[0]),
+      gs4marks: parseInt(fields.gs4marks[0]),
+      ProfileImage: image64,
+      essayMarks: parseInt(fields.essayMarks[0]),
+      prelimsScoreGs: parseInt(fields.prelimsScoreGs[0]),
+      prelimsScoreCsat: parseInt(fields.prelimsScoreCsat[0]),
+      optionalSub: fields.optionalSub[0],
+
+      optional1Marks: parseInt(fields.optional1Marks[0]),
+      optional2Marks: parseInt(fields.optional2Marks[0]),
+      Remarks: fields.Remarks[0],
+    });
+    const savedTopper = await newTopper.save();
+  } catch (err) {
+    console.log(err);
   }
-
-  const compressedBuffer = await sharp(files.profileImage[0].filepath)
-    .resize({ width: 250, height: 250 })
-    .withMetadata()
-    .jpeg({ quality: 80 }) // Adjust quality as needed
-    .toBuffer();
-
-  // const filename = new Date().getTime() + "-" + req.file.originalname;
-  // const newFilePath = path.join(UPLOAD_PATH, filename);
-
-  // const buffer = await sharp(req.file.buffer)
-  //   .resize()
-  //   .jpeg({ quality: 50 })
-  //   .toBuffer();
-
-  const image64 = compressedBuffer.toString("base64");
-
-  let newTopper = new topper({
-    name: fields.name[0],
-    rank: parseInt(fields.rank[0]),
-    year: parseInt(fields.year[0]),
-    gs1marks: parseInt(fields.gs1marks[0]),
-    gs2marks: parseInt(fields.gs2marks[0]),
-    gs3marks: parseInt(fields.gs3marks[0]),
-    gs4marks: parseInt(fields.gs4marks[0]),
-    ProfileImage: image64,
-    essayMarks: parseInt(fields.essayMarks[0]),
-    prelimsScoreGs: parseInt(fields.prelimsScoreGs[0]),
-    prelimsScoreCsat: parseInt(fields.prelimsScoreCsat[0]),
-    optionalSub: fields.optionalSub[0],
-
-    optional1Marks: parseInt(fields.optional1Marks[0]),
-    optional2Marks: parseInt(fields.optional2Marks[0]),
-    Remarks: fields.Remarks[0],
-  });
-  const savedTopper = await newTopper.save();
-
   // await sharp(req.file.path)
   //   .resize()
   //   .jpeg({ quality: 50 })
