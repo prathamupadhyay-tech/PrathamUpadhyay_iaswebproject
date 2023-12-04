df1696db7359ab108f34c845aa13ce01d427e594
prathamupadhyay-tech <pratham27900@gmail.com>
2023-12-04 13:29:19 +0530

    changes to image

--- pages/api/AddTopper.js
+++ pages/api/AddTopper.js
@@ -7,13 +7,13 @@ import path from "path";
 
 import formidable, { errors as formidableErrors } from "formidable";
 
-// export const config = {
-//   api: {
-//     bodyParser: [
-//       api
-//     ],
-//   },
-// };
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "10mb",
+    },
+  },
+};
 
 // const upload = multer({
 //   storage: multer.diskStorage({
@@ -32,25 +32,25 @@ const saveTopper = async (req, res) => {
     //   return res.status(400).json({ message: "Image file is required" });
     // }
 
-    const startIndex = req.body.profileImage.indexOf(",") + 1;
-    const base64ImageData = req.body.profileImage.slice(startIndex);
-    const buffer = Buffer.from(base64ImageData, "base64");
+    // const startIndex = req.body.profileImage.indexOf(",") + 1;
+    // const base64ImageData = req.body.profileImage.slice(startIndex);
+    // const buffer = Buffer.from(base64ImageData, "base64");
 
-    const compressedBuffer = await sharp(buffer)
-      .resize({ width: 250, height: 250 })
-      .withMetadata()
-      .jpeg({ quality: 80 }) // Adjust quality as needed
-      .toBuffer();
-
-    // const filename = new Date().getTime() + "-" + req.file.originalname;
-    // const newFilePath = path.join(UPLOAD_PATH, filename);
-
-    // const buffer = await sharp(req.file.buffer)
-    //   .resize()
-    //   .jpeg({ quality: 50 })
+    // const compressedBuffer = await sharp(buffer)
+    //   .resize({ width: 250, height: 250 })
+    //   .withMetadata()
+    //   .jpeg({ quality: 80 }) // Adjust quality as needed
     //   .toBuffer();
-  
-    const image64 = compressedBuffer.toString("base64");
+
+    // // const filename = new Date().getTime() + "-" + req.file.originalname;
+    // // const newFilePath = path.join(UPLOAD_PATH, filename);
+
+    // // const buffer = await sharp(req.file.buffer)
+    // //   .resize()
+    // //   .jpeg({ quality: 50 })
+    // //   .toBuffer();
+
+    // const image64 = compressedBuffer.toString("base64");
 
     let newTopper = new topper({
       name: req.body.name,
@@ -60,7 +60,7 @@ const saveTopper = async (req, res) => {
       gs2marks: req.body.gs2marks,
       gs3marks: req.body.gs3marks,
       gs4marks: req.body.gs4marks,
-      ProfileImage: image64,
+      ProfileImage: req.body.profileImage,
       essayMarks: req.body.essayMarks,
       prelimsScoreGs: req.body.prelimsScoreGs,
       prelimsScoreCsat: req.body.prelimsScoreCsat,
@@ -71,7 +71,7 @@ const saveTopper = async (req, res) => {
       Remarks: req.body.Remarks,
     });
     const savedTopper = await newTopper.save();
-    
+
     return res.status(200).json({ message: "successful" });
   } catch (err) {
     console.log(err);
