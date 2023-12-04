801ffe53523e4db81d6214ea554f0f68e9834b9d
prathamupadhyay-tech <pratham27900@gmail.com>
2023-12-04 12:34:00 +0530

    changes

--- pages/api/AddTopper.js
+++ pages/api/AddTopper.js
@@ -4,10 +4,8 @@ import multer from "multer";
 import sharp from "sharp";
 import fs from "fs";
 import path from "path";
-import { createRouter, expressWrapper } from "next-connect";
+
 import formidable, { errors as formidableErrors } from "formidable";
-const UPLOAD_PATH = path.join("public", "uploads");
-const router = createRouter();
 
 export const config = {
   api: {
@@ -26,53 +24,17 @@ export const config = {
 //   }),
 // });
 
-router
-  // .use(upload.single("profileImage"))
-
-  .post(async (req, res) => {
-    const form = formidable({});
-    let fields;
-    let files;
-    try {
-      [fields, files] = await form.parse(req);
-      console.log(fields);
-      console.log(files);
-      await saveTopper(fields, files);
-      res.status(200).json({ message: "saved" });
-    } catch (err) {
-      // example to check for a very specific error
-      if (err.code === formidableErrors.maxFieldsExceeded) {
-      }
-      console.log(err);
-      console.error(err);
-      res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
-      res.end(String(err));
-      return;
-    }
-    // try {
-    //   const form = new formidable.IncomingForm();
-
-    //   form.parse(req, async function (err, fields, files) {
-    //     await saveTopper(files.file);
-    //     return res.status(201).json({ message: "saved" });
-    //   });
-    // } catch (err) {
-    //   console.log(err);
-    //   res.status(500).json({ message: err });
-    // }
-  });
-
 const saveTopper = async (fields, files) => {
   try {
     if (!files) {
       return res.status(400).json({ message: "Image file is required" });
     }
 
-    // const compressedBuffer = await sharp(files.profileImage[0].filepath)
-    //   .resize({ width: 250, height: 250 })
-    //   .withMetadata()
-    //   .jpeg({ quality: 80 }) // Adjust quality as needed
-    //   .toBuffer();
+    const compressedBuffer = await sharp(files.profileImage[0].filepath)
+      .resize({ width: 250, height: 250 })
+      .withMetadata()
+      .jpeg({ quality: 80 }) // Adjust quality as needed
+      .toBuffer();
 
     // const filename = new Date().getTime() + "-" + req.file.originalname;
     // const newFilePath = path.join(UPLOAD_PATH, filename);
@@ -82,7 +44,7 @@ const saveTopper = async (fields, files) => {
     //   .jpeg({ quality: 50 })
     //   .toBuffer();
 
-    // const image64 = compressedBuffer.toString("base64");
+    const image64 = compressedBuffer.toString("base64");
 
     let newTopper = new topper({
       name: fields.name[0],
@@ -92,7 +54,7 @@ const saveTopper = async (fields, files) => {
       gs2marks: parseInt(fields.gs2marks[0]),
       gs3marks: parseInt(fields.gs3marks[0]),
       gs4marks: parseInt(fields.gs4marks[0]),
-
+      ProfileImage: image64,
       essayMarks: parseInt(fields.essayMarks[0]),
       prelimsScoreGs: parseInt(fields.prelimsScoreGs[0]),
       prelimsScoreCsat: parseInt(fields.prelimsScoreCsat[0]),
@@ -125,9 +87,37 @@ const saveTopper = async (fields, files) => {
   // }
 };
 
-export default router.handler({
-  onError: (err, req, res) => {
-    console.error(err.stack);
-    res.status(err.statusCode || 500).end(err.message);
-  },
-});
+const post = async (req, res) => {
+  const form = formidable({});
+  let fields;
+  let files;
+  try {
+    [fields, files] = await form.parse(req);
+    console.log(fields);
+    console.log(files);
+    await saveTopper(fields, files);
+    res.status(200).json({ message: "saved" });
+  } catch (err) {
+    // example to check for a very specific error
+    if (err.code === formidableErrors.maxFieldsExceeded) {
+    }
+    console.log(err);
+    console.error(err);
+    res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
+    res.end(String(err));
+    return;
+  }
+  // try {
+  //   const form = new formidable.IncomingForm();
+
+  //   form.parse(req, async function (err, fields, files) {
+  //     await saveTopper(files.file);
+  //     return res.status(201).json({ message: "saved" });
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  //   res.status(500).json({ message: err });
+  // }
+};
+
+export default post;
