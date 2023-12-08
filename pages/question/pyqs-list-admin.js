ba7954836fb001f0945966a9ad32360bea58ed97
prathamupadhyay-tech <pratham27900@gmail.com>
2023-12-09 01:20:28 +0530

    Fixed Bug

--- pages/question/pyqs-list-admin.js
+++ pages/question/pyqs-list-admin.js
@@ -11,7 +11,7 @@ const PyqsListadmin = ({ questions }) => {
           <div className={styles.srNoDiv}>
             <h3>sr. no</h3>
           </div>
-          <div  className={styles.questionTextDiv}>
+          <div className={styles.questionTextDiv}>
             <h3>Question text</h3>
           </div>
           <div className={styles.solutionTextDiv}>
@@ -20,18 +20,18 @@ const PyqsListadmin = ({ questions }) => {
         </div>
         {questions.map((data, index) => {
           return (
-            <div className={styles.questionContainer}>
-                <div  className={styles.srNoDiv1}>
-                    <p>{index+1}.</p>
-                </div>
+            <div key={index} className={styles.questionContainer}>
+              <div className={styles.srNoDiv1}>
+                <p>{index + 1}.</p>
+              </div>
               <div
-              className={styles.questionTextDiv}
-                key={index}
+                className={styles.questionTextDiv}
                 dangerouslySetInnerHTML={{ __html: data.question }}
               ></div>
-              <div className={styles.solutionTextDiv}  dangerouslySetInnerHTML={{ __html: data.solution }}>
-
-              </div>
+              <div
+                className={styles.solutionTextDiv}
+                dangerouslySetInnerHTML={{ __html: data.solution }}
+              ></div>
             </div>
           );
         })}
